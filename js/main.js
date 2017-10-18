76cff5846ecade60075f649982c16c496f04da51
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-10-18 09:27:10 +0200

    initial repo commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var menuToggle = document.querySelector('.menu-toggle');
+var menuMain = document.querySelector('.menu-main');
+
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('menu-accentcolor');
+    menuMain.classList.toggle('menu-visible');
+}
+
